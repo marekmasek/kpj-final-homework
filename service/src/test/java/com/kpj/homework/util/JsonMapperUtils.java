e1f53112bdefd001ff50e0c1675dffb388fb4e46
Marek Mašek <24.mara@seznam.cz>
2022-05-25 01:06:53 +0200

    removed sneakythrows

--- service/src/test/java/com/kpj/homework/util/JsonMapperUtils.java
+++ service/src/test/java/com/kpj/homework/util/JsonMapperUtils.java
@@ -1,13 +1,12 @@
 package com.kpj.homework.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import lombok.SneakyThrows;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 public class JsonMapperUtils {
 
-    @SneakyThrows
-    public static String object2Json(Object object) {
+    public static String object2Json(Object object) throws JsonProcessingException {
         return Jackson2ObjectMapperBuilder.json()
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .build()
